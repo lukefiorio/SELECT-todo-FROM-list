f203fd6450468112e337ed3f1599fd549a905cee
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-27 17:58:24 -1000

    assignment completed

---
+++ todo_app.sql
@@ -0,0 +1,58 @@
+DROP TABLE IF EXISTS tasks;
+
+CREATE TABLE tasks (
+  id serial NOT NULL primary key,
+  title varchar(255) NOT NULL,
+  description text,
+  created_at timestamp without time zone NOT NULL DEFAULT now(),
+  updated_at timestamp without time zone,
+  completed boolean NOT NULL DEFAULT false
+);
+
+ALTER TABLE tasks DROP completed;
+ALTER TABLE tasks ADD completed_at timestamp DEFAULT NULL;
+ALTER TABLE tasks ALTER COLUMN updated_at SET DEFAULT now();
+
+INSERT INTO tasks (title, description, updated_at) VALUES ('Study SQL', 'Complete this exerices', now());
+INSERT INTO tasks (title, description, updated_at) VALUES ('Study PostgreSQL', 'Read all the documentation', now());
+
+SELECT title
+FROM tasks 
+WHERE completed_at is NULL;
+
+UPDATE tasks
+SET completed_at = now()
+WHERE title = 'Study SQL';
+
+SELECT title, description
+FROM tasks 
+WHERE completed_at is NULL;
+
+SELECT *
+FROM tasks
+ORDER BY created_at DESC;
+
+
+INSERT INTO tasks (title, description, updated_at) VALUES ('mistake 1', 'a test entry', now());
+INSERT INTO tasks (title, description, updated_at) VALUES ('mistake 2', 'another test entry', now());
+INSERT INTO tasks (title, description, updated_at) VALUES ('third mistake', 'another test entry', now());
+
+SELECT title
+FROM tasks
+WHERE title LIKE '%mistake%';
+
+DELETE
+FROM tasks
+WHERE title = 'mistake 1';
+
+SELECT title, description
+FROM tasks
+WHERE title LIKE '%mistake%';
+
+DELETE
+FROM tasks
+WHERE title LIKE '%mistake%';
+
+SELECT * 
+FROM tasks
+ORDER BY title ASC;
